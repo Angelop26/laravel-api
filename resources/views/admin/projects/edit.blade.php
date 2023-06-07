8a7254e16be4c6566b710231b40bf6948ac7124f
Angelop26 <angeloprospero26@gmail.com>
2023-06-07 15:51:03 +0200

    add: updated crud and bonus 1 completed

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -8,6 +8,19 @@
         <label for="title" class="form-label">title</label>
         <input type="text" class="form-control" id="title" name="title" value="{{old('title', $project->title)}}">
     </div>
+
+    <div class="mb-3">
+        <label for="type">type</label>
+        <select class="form-select" aria-label="Default select example" id="type" name="type_id" >
+            
+            <option value=""></option>
+            @foreach ($types as $type)
+                <option @selected($type->id == old('type_id',$project->$type?->id))  value="{{$type->id}}"> {{$type->name}} </option>
+            @endforeach
+            
+        </select>
+    </div>
+    
     <div class="mb-3">
         <label for="content" class="form-label">content</label>
         <textarea class="form-control" value="{{old('content', $project->content)}}" id="content" name='content' rows="3" ></textarea>

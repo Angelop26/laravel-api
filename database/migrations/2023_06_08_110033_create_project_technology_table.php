b271a19e453ffe880441619575d03b3ca0c84adf
Angelop26 <angeloprospero26@gmail.com>
2023-06-08 13:09:09 +0200

    add: technology's model and migration, pivot's migration and updated models

---
+++ database/migrations/2023_06_08_110033_create_project_technology_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_technology', function (Blueprint $table) {
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->on('projects')->references('id')->onDelete('cascade');
+        
+            $table->unsignedBigInteger('technology_id');
+            $table->foreign('technology_id')->on('technologies')->references('id')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_technology');
+    }
+};

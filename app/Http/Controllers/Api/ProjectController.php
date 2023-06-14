e88ed1313df0d8679711058f3cdcedc06d230a57
Angelop26 <angeloprospero26@gmail.com>
2023-06-14 19:02:55 +0200

    bonus 3 completed

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
+use App\Models\Technology;
 use GrahamCampbell\ResultType\Success;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::all();
+        $projects = Project::with('technologies', 'type')->paginate(6);
         return response()->json([
             'success' => true,
             'results' => $projects
